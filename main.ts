803bf8e8f5ca76d7ec28e5eaa8d0707c2679688f
3A732094 <73567050+3A732094@users.noreply.github.com>
2021-06-21 06:25:48 +0800

    增加了一些可以讓程式碼不會有問題的

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showString(mypass)
-    mypass = ""
-})
-let item = ""
+let wrong = 0
 let mypass = ""
+let item = ""
 let password = "1234"
 I2C_LCD1602.LcdInit(39)
 I2C_LCD1602.BacklightOn()
@@ -18,37 +15,64 @@ DigitalPin.P9,
 DigitalPin.P10
 )
 pins.setAudioPin(AnalogPin.P5)
-I2C_LCD1602.ShowString("Mypass=", 0, 0)
+I2C_LCD1602.ShowString("Be careful!", 0, 0)
+I2C_LCD1602.ShowString("My password=", 0, 1)
 basic.forever(function () {
     item = keypad.getKeyString()
-    if (item != "") {
-        mypass = "" + mypass + item
-        basic.showString(item)
-        I2C_LCD1602.ShowString("Mypass=", 0, 0)
-        I2C_LCD1602.ShowString(mypass, 7, 0)
-        if (mypass.length == 4) {
-            if (mypass == password) {
-                I2C_LCD1602.clear()
-                I2C_LCD1602.ShowString("corrrct", 0, 0)
-                pins.analogWritePin(AnalogPin.P12, 1023)
-            } else {
-                I2C_LCD1602.clear()
-                I2C_LCD1602.ShowString("wrong", 0, 0)
-                pins.analogWritePin(AnalogPin.P13, 1023)
-                basic.pause(2000)
-                I2C_LCD1602.clear()
-                I2C_LCD1602.BacklightOff()
-                basic.pause(500)
-                I2C_LCD1602.BacklightOn()
-                mypass = ""
-                pins.analogWritePin(AnalogPin.P5, 1023)
-                music.setVolume(255)
-                music.startMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.Once)
-                basic.pause(5000)
-                music.stopAllSounds()
+    if (item == "A" || (item == "B" || (item == "D" || item == "#"))) {
+        I2C_LCD1602.clear()
+        I2C_LCD1602.ShowString("Door closing!", 0, 0)
+        basic.pause(5000)
+        mypass = ""
+        I2C_LCD1602.clear()
+    } else if (item == "C") {
+        I2C_LCD1602.clear()
+        mypass = ""
+        I2C_LCD1602.ShowString("Be careful!", 0, 0)
+        I2C_LCD1602.ShowString("My password=", 0, 1)
+    } else if (mypass == "***") {
+        I2C_LCD1602.on()
+        I2C_LCD1602.BacklightOn()
+        I2C_LCD1602.clear()
+        mypass = ""
+    } else {
+        I2C_LCD1602.ShowString("Be careful!", 0, 0)
+        I2C_LCD1602.ShowString("My password=", 0, 1)
+        if (item != "") {
+            mypass = "" + mypass + item
+            basic.showString(item)
+            I2C_LCD1602.ShowString(mypass, 12, 1)
+            if (mypass.length == 4) {
+                if (mypass == password) {
+                    pins.analogWritePin(AnalogPin.P13, 0)
+                    I2C_LCD1602.clear()
+                    I2C_LCD1602.ShowString("Door open", 0, 0)
+                    pins.analogWritePin(AnalogPin.P12, 1023)
+                } else {
+                    wrong = wrong + 1
+                    I2C_LCD1602.clear()
+                    I2C_LCD1602.ShowString("wrong", 0, 0)
+                    pins.analogWritePin(AnalogPin.P13, 1023)
+                    basic.pause(2000)
+                    I2C_LCD1602.clear()
+                    pins.analogWritePin(AnalogPin.P13, 0)
+                    mypass = ""
+                    pins.analogWritePin(AnalogPin.P5, 1023)
+                    music.setVolume(255)
+                    music.startMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.Once)
+                    music.playMelody("C - C - C - C - ", 120)
+                    basic.pause(500)
+                    music.stopAllSounds()
+                }
+                if (wrong == 1) {
+                    I2C_LCD1602.ShowString("You are a thief", 0, 0)
+                    basic.pause(5000)
+                    I2C_LCD1602.clear()
+                    I2C_LCD1602.BacklightOff()
+                    I2C_LCD1602.off()
+                }
+                wrong = 0
             }
-        } else {
-            I2C_LCD1602.ShowString("length must be 4", 0, 1)
         }
     }
 })
